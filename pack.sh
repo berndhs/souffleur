72513fa64a5adc4717b0094a00bc1b8be17b24d8
Bernd Stramm <bernd.stramm@gmail.com>
2010-12-02 13:48:32 -0500

    grab version from source

--- pack.sh
+++ pack.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 NAME=souffleur
-VERSION=0.0.7
+VERSION=`grep "ProgramVersion::VersionNumber" src/version.cpp \
+        | awk '{print $3;}' \
+        | sed s/[\(\"\;\)]//g`
 PACKDIR=${HOME}/packaging/sched
 
 makearchive.sh ${NAME}-${VERSION} master
