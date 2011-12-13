2b9108dd471139fffed1e9e079e03cce1fd8ea52
Bernd Stramm <bernd.stramm@gmail.com>
2011-12-13 11:10:00 -0500

    update packing versions, note changes

--- pack-all.sh
+++ pack-all.sh
@@ -24,6 +24,7 @@ function list_contains_count () {
   done
 }
 
+set -x
 ALL_ARGS=$*
 NAME=souffleur
 CHANGELOG=${NAME}.changes
@@ -48,9 +49,9 @@ cd $TEMPDIR
 
 tar zxf $TARFILE
 
-mad -t harmattan-platform-api dpkg-source -b ${ARCHIVE}
+#mad -t harmattan-platform-api dpkg-source -b ${ARCHIVE}
 
-#dpkg-source -b ${ARCHIVE}
+dpkg-source -b ${ARCHIVE}
 
 cd ..
 
